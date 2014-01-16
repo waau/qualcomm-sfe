42801d74ca85000b4e12a3a387bb74db6dc51ce4
Matthew McClintock <mmcclint@codeaurora.org>
2014-01-16 16:47:38 -0600

    Revert "[shortcut-fe] fix null dereference for non-PPPoE case"
    This reverts commit 3efd100562d873df3e42c0af410597187a0a2a3c.

--- shortcut-fe/sfe_cm.c
+++ shortcut-fe/sfe_cm.c
@@ -441,7 +441,7 @@ static unsigned int sfe_cm_ipv4_post_routing_hook(unsigned int hooknum,
 	sic.src_mtu = 1500;
 	sic.dest_mtu = 1500;
 
-	if (last_pppox_sock && last_pppox_sock->pppoe_dev == in->name) {
+	if (last_pppox_sock->pppoe_dev == in->name) {
 		struct sock *sk = &last_pppox_sock->sk;
 
 		if (sk->sk_family == PF_PPPOX && sk->sk_protocol == PX_PROTO_OE) {
